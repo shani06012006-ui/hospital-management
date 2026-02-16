56eb0a0ae7cf0f6df66c595c82a5754c8593bd57
shani06012006 <shani06012006@gmail.com>
2026-02-16 12:24:47 +0530

    updated files

--- hospital_management/hospital/static/hospital/main.js
+++ hospital_management/hospital/static/hospital/main.js
@@ -1,6 +1,5 @@
 console.log("MedPlus Management System Active");
 
-// Add a simple fade-in effect to the container
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.main-container');
     container.style.opacity = 0;
